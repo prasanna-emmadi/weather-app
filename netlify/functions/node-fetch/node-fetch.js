250b6fedaec57a83c55fac2ac819f0aaa3435a41
Prasanna Emmadi <prasanna.177@gmail.com>
2022-02-17 00:05:04 +0200

    Adding netlify serverless fn
    for cors issues

---
+++ netlify/functions/node-fetch/node-fetch.js
@@ -0,0 +1,63 @@
+const fetch = require("node-fetch");
+
+const BASE_URL = "https://dataservice.accuweather.com";
+const CITY_URL = `${BASE_URL}/locations/v1/cities/search`;
+const DAILY_URL = `${BASE_URL}/forecasts/v1/daily/1day`;
+
+const fetchData = async (url) => {
+  const response = await fetch(url, {
+    headers: { Accept: "application/json" },
+  });
+  if (!response.ok) {
+    // NOT res.status >= 200 && res.status < 300
+    return { statusCode: response.status, body: response.statusText };
+  }
+};
+
+const handler = async function (event, context) {
+  const city = event.queryStringParameters.city || "Oulu";
+  const apikey = event.queryStringParameters.apikey || "";
+  console.log({ city, apikey });
+
+  try {
+    const url = `${CITY_URL}?apikey=${apikey}&q=${city}&language=en-us&details=true`;
+
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {},
+    });
+
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      console.log("response fail 1");
+      return { statusCode: response.status, body: response.statusText };
+    }
+    const citydata = await response.json();
+    const locationKey = citydata[0].Details.CanonicalLocationKey;
+    const dailyurl = `${DAILY_URL}/${locationKey}?apikey=${apikey}`;
+    const response1 = await fetch(dailyurl, {
+      headers: { Accept: "application/json" },
+    });
+    if (!response1.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText };
+    }
+
+    const weatherdata = await response1.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ DailyForecasts: weatherdata.DailyForecasts }),
+    };
+  } catch (error) {
+    // output to netlify function log
+    console.log(error);
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message }),
+    };
+  }
+};
+
+module.exports = { handler };
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,37 +1,8 @@
-const BASE_URL = "https://dataservice.accuweather.com";
-const CITY_URL = `${BASE_URL}/locations/v1/cities/search`;
-const DAILY_URL = `${BASE_URL}/forecasts/v1/daily/1day`;
-
-const getResponseJson = (url) => {
-  return fetch(url, {
-    method: "GET",
-    headers: {},
-  })
-    .then((res) => {
-      return res.json();
-    })
-    .catch((e) => {
-      console.log("error in fetch");
-      console.log(e);
-    });
-};
-
 const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-console.log({ apiKey });
-
-const getCityLocationKey = (city) => {
-  const url = `${CITY_URL}?apikey=${apiKey}&q=${city}&language=en-us&details=true`;
-  return getResponseJson(url).then((responseJson) => {
-    const locationKey = responseJson[0].Details.CanonicalLocationKey;
-    return Promise.resolve(locationKey);
-  });
-};
 
 export const getDailyForecast = (city) => {
-  return getCityLocationKey(city).then((locationKey) => {
-    const url = `${DAILY_URL}/${locationKey}?apikey=${apiKey}`;
-    return getResponseJson(url).then((responseJson) => {
-      return Promise.resolve(responseJson.DailyForecasts);
-    });
-  });
+  const url = `/.netlify/functions/node-fetch?city=${city}&apikey=${apiKey}`;
+  return fetch(url, { headers: { accept: "Accept: application/json" } }).then(
+    (x) => x.json()
+  );
 };
